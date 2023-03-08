25601a365b0308177987b5ce043a051f4861f870
vinibiaz <vinibiaz@gmail.com>
2023-03-08 08:43:50 -0500

    Add edit, delete, new

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const methodOverride = require('method-override'); //add when I install override to update data
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/yeld-camp'//, {
@@ -27,18 +28,48 @@ const Campground = require('./models/campground');
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({extended: true}));//make my req.body not blank!!! really important-- used on post
+app.use(methodOverride('_method'));//add override
+
 app.get('/', (req, res)=>{
     res.render('home');
 })
 /**
- * Making a data Hardcoding
+ * Making a data 
  */
-app.get('/makecampground', async(req, res)=>{
-    const camp = new Campground({title: 'My Backyard', description: "cheap camping!"});
-    await camp.save();
-    res.send(camp);
+app.get('/campgrounds', async(req, res)=>{
+    const campgrounds = await Campground.find({});
+    res.render('campground/index', {campgrounds:campgrounds});
 })
 
-app.listen(3000, ()=>{
-    console.log("Serving on port 3000");
+app.get('/campgrounds/new', (req,res)=>{
+    res.render('campground/new');
+})
+app.get('/campgrounds/:id', async(req,res)=>{
+    const campground = await Campground.findById((req.params.id));
+    res.render('campground/show', {campground});
+})
+app.post('/campgrounds', async(req,res)=>{
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+})
+
+app.get('/campgrounds/:id/edit', async(req,res)=>{
+    const campground = await Campground.findById((req.params.id));
+    res.render('campground/edit', {campground});
+})
+app.put('/campgrounds/:id', async (req, res)=>{
+    const {id} = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+    res.redirect(`/campgrounds/${campground._id}`);
+})
+app.delete('/campgrounds/:id', async (req, res)=>{
+    const {id} = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+})
+
+app.listen(1111, ()=>{
+    console.log("Serving on port 1111");
 })
---
+++ seeds/index.js
@@ -0,0 +1,101 @@
+/**
+ * Connect to mongoose and connect my models
+ */
+const descriptors = [
+    'Forest',
+    'Ancient',
+    'Petrified',
+    'Roaring',
+    'Cascade',
+    'Tumbling',
+    'Silent',
+    'Redwood',
+    'Bullfrog',
+    'Maple',
+    'Misty',
+    'Elk',
+    'Grizzly',
+    'Ocean',
+    'Sea',
+    'Sky',
+    'Dusty',
+    'Diamond'
+]
+const places = [
+    'Flats',
+    'Village',
+    'Canyon',
+    'Pond',
+    'Group Camp',
+    'Horse Camp',
+    'Ghost Town',
+    'Camp',
+    'Dispersed Camp',
+    'Backcountry',
+    'River',
+    'Creek',
+    'Creekside',
+    'Bay',
+    'Spring',
+    'Bayshore',
+    'Sands',
+    'Mule Camp',
+    'Hunting Camp',
+    'Cliffs',
+    'Hollow'
+]
+
+
+const path = require('path');
+const mongoose = require('mongoose');
+
+const Campground = require('../models/campground');
+
+//adding cities file
+const cities = require('./cities');
+//const {places, descriptors} = require('./seedHelpers.js');
+
+mongoose.connect('mongodb://localhost:27017/yeld-camp'//, {
+    //userNewUrlParser: true
+    //userCreatedIndex: true
+   // userUnifiedTopology: true
+//}
+);
+ //Connecting with database
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "conection error:"));
+db.once("open", ()=>{
+    console.log("Database connected!!");
+})
+
+const sample = array =>{
+    const teste =array[Math.floor(Math.random() * array.lenght)];
+}
+
+const data = descriptors[Math.floor(Math.random() * descriptors.length)];
+//
+// Adding only one (TESTING)
+//const seedDB = async() => {
+//    await Campground.deleteMany({});//frist I'll delete everything
+//    const c = new Campground({title:'purple field'});
+//    await c.save();
+//}
+
+const seedDB = async() => {
+    await Campground.deleteMany({});//first I'll delete everything
+    for(let i =0; i <50; i++){
+        const random1000 = Math.floor(Math.random() * 1000);
+        const descriptorsData = descriptors[Math.floor(Math.random() * descriptors.length)]
+        const placesData = places[Math.floor(Math.random() * places.length)]
+        const camp = new Campground({
+            location: `${cities[random1000].city},${cities[random1000].state}`,
+            title :`${descriptorsData} ${placesData}`
+        })
+        await camp.save();
+    }
+}
+console.log(data);
+seedDB().then(() => {
+    mongoose.connection.close();
+    console.log('Database closed');
+})
